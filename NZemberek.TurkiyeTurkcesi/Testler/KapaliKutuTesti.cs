6c9ec5f506ecb8e71fa10d6aed88284f2bf26da2
tankut.tekeli <tankut.tekeli@69e49649-1523-0410-a81d-1db19b50af4b>
2006-12-30 21:05:58 +0000

    TR kutuphanesine KapaliKutuTesti eklendi. Kaynaklar eklenince test calisacak.

---
+++ NZemberek.TurkiyeTurkcesi/Testler/KapaliKutuTesti.cs
@@ -0,0 +1,56 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Zemberek Doðal Dil Ýþleme Kütüphanesi.
+ *
+ * The Initial Developer of the Original Code is
+ * Ahmet A. Akýn, Mehmet D. Akýn.
+ * Portions created by the Initial Developer are Copyright (C) 2006
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Mert Derman
+ *   Tankut Tekeli
+ * ***** END LICENSE BLOCK ***** */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using net.zemberek.erisim;
+using NUnit.Framework;
+
+
+namespace NZemberek.TurkiyeTurkcesi.Testler
+{
+    [TestFixture]
+    public class KapaliKutuTesti
+    {
+        private static Zemberek zemberek;
+
+        [SetUp]
+        public void baslangic()
+        {
+            zemberek = new Zemberek();
+        }
+
+        [Test]
+        public void testCozumle1()
+        {
+            string str = "kedi";
+            Assert.IsTrue(zemberek.kelimeDenetle(str));
+            string[] sonuc = zemberek.kelimeCozumle(str);
+            Assert.AreEqual(1, sonuc.Length);
+            Assert.AreEqual("kedi", sonuc[0]);
+        }
+    }
+}
