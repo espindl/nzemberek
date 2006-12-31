2266401f348955da6f6a39842cbe482396f062f8
spinodal <spinodal@69e49649-1523-0410-a81d-1db19b50af4b>
2006-12-31 15:34:21 +0000

    Dil araclarina tasinanlar...

---
+++ NZemberek.DilAraclari/araclar.turkce/TurkceMetinOkuyucu.cs
@@ -0,0 +1,85 @@
+/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Zemberek Doðal Dil Ýþleme Kütüphanesi.
+ *
+ * The Initial Developer of the Original Code is
+ * Ahmet A. Akýn, Mehmet D. Akýn.
+ * Portions created by the Initial Developer are Copyright (C) 2006
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Mert Derman
+ *   Tankut Tekeli
+ * ***** END LICENSE BLOCK ***** */
+
+// V0.1
+using System;
+using System.Collections;
+using System.Text;
+using System.IO;
+//using net.zemberek.istatistik;
+using log4net;
+
+
+namespace net.zemberek.araclar.turkce
+{
+    public class TurkceMetinOkuyucu
+    {
+        private static readonly ILog logger = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);		
+        //private Istatistikler istatistikler = null;
+
+        public String[] MetinOku(String path)
+        {
+            String[] kelimeler;
+            TurkishTokenStream stream = new TurkishTokenStream(path, "ISO-8859-9");
+            ArrayList list = new ArrayList();
+            while (true)
+            {
+                String str = stream.nextWord();
+                if (str == null) break;
+                list.Add(str);
+            }
+            if (logger.IsInfoEnabled)
+                logger.Info(" Metin kelime sayisi : " + list.Count);
+            kelimeler = new String[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                kelimeler[i] = (String)list[i];
+            }
+            return kelimeler;
+        }
+
+        public String[] MetinOku(Stream sr)
+        {
+            String[] kelimeler;
+            TurkishTokenStream stream = new TurkishTokenStream(new StreamReader(sr,System.Text.Encoding.GetEncoding("ISO-8859-9")));
+            ArrayList list = new ArrayList();
+            while (true)
+            {
+                String str = stream.nextWord();
+                if (str == null) break;
+                list.Add(str);
+            }
+            if (logger.IsInfoEnabled)
+                logger.Info(" Metin kelime sayisi : " + list.Count);
+            kelimeler = new String[list.Count];
+            for (int i = 0; i < list.Count; i++)
+            {
+                kelimeler[i] = (String)list[i];
+            }
+            return kelimeler;
+        }
+    }
+}
+
---
+++ NZemberek.DilAraclari/bilgi.araclar/IkiliKokYazici.cs
@@ -0,0 +1,74 @@
+﻿/* ***** BEGIN LICENSE BLOCK *****
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is Zemberek Doğal Dil İşleme Kütüphanesi.
+ *
+ * The Initial Developer of the Original Code is
+ * Ahmet A. Akın, Mehmet D. Akın.
+ * Portions created by the Initial Developer are Copyright (C) 2006
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ *   Mert Derman
+ *   Tankut Tekeli
+ * ***** END LICENSE BLOCK ***** */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using net.zemberek.yapi;
+using net.zemberek.yapi.kok;
+
+namespace net.zemberek.bilgi.araclar
+{
+    public class IkiliKokYazici: KokYazici
+    {
+      BinaryWriter   dos;
+
+    public IkiliKokYazici(String dosyaAdi) {
+        FileStream fos = new FileStream(dosyaAdi,FileMode.Create); //TODO mı Append mi
+        dos = new BinaryWriter(fos);
+    }
+
+    public void yaz(List<Kok> kokler) {
+
+        foreach (Kok kok in kokler) {
+            // Kök içerigi
+            dos.Write(kok.icerik());
+
+            // asil icerik ozel karakterler barindiran koklerde olur. yoksa bos string yaz.
+            if (kok.asil() != null) {
+                // Kök asil içerigi
+                dos.Write(kok.asil());
+            } else
+                dos.Write("");
+
+            // Kök tipi
+            dos.Write(kok.tip().ToString());
+
+            dos.Write(kok.KisaltmaSonSeslisi);
+
+            KokOzelDurumu[] ozd = kok.ozelDurumDizisi();
+            dos.Write(ozd.Length);
+            foreach (KokOzelDurumu ozelDurum in ozd) {
+                dos.Write(ozelDurum.indeks());
+            }
+            // kullanim frekansi
+            dos.Write(kok.Frekans);
+
+        }
+        dos.Close();
+    }
+    }
+}
