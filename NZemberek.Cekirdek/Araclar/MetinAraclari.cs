3f0ba60b2406e32657ef58a9403d1117e4c63b75
spinodal <spinodal@69e49649-1523-0410-a81d-1db19b50af4b>
2007-01-06 21:30:17 +0000

    MetinAraclari'nda ufak degisiklikler ve  TestMetinAraclari

--- NZemberek.Cekirdek/Araclar/MetinAraclari.cs
+++ NZemberek.Cekirdek/Araclar/MetinAraclari.cs
@@ -22,13 +22,9 @@
  *   Mert Derman
  *   Tankut Tekeli
  * ***** END LICENSE BLOCK ***** */
-
-// V 0.1
 using System;
 using System.Collections.Generic;
 using System.Text;
-//using log4net;
-
 
 namespace NZemberek.Cekirdek.Araclar
 {
@@ -49,113 +45,6 @@ namespace NZemberek.Cekirdek.Araclar
         {
             int maxDif = Math.Max(source.Length, target.Length);
             return editDistance(source, target, maxDif);
-            //return editDistanceMert(source, target, maxDif);
-        }
-
-        /**
-         * Degistirilmis Levenshtein Edit Dist. algoritması. transpozisyonları da 1 düzeltme mesafesi
-         * olarak hesaplar. Uzaklik limit degerden buyuk olursa islem kesilir. (Alphan)
-         *
-         * @param source
-         * @param target
-         * @param limit
-         * @return iki kelime arasindaki mesafe, tamsayi cinsinden. kucuk rakamlar daha buyuk benzerligi gosterir.
-         */
-        public static int editDistance(String source, String target, int limit)
-        {
-            // Step 1
-            int n = source.Length;
-            int m = target.Length;
-            int disarda = limit + 1;
-
-            if (n == 0 || m == 0)
-                return Math.Max(m, n);
-
-            if (Math.Abs(m - n) > limit)
-                return disarda;
-
-            int[,] matrix = new int[n + 1, m + 1];
-
-            // Step 2
-            int ust;
-            ust = Math.Min(limit, n);
-            for (int i = 0; i <= ust; i++)
-                matrix[i, 0] = i;
-            ust = Math.Min(limit, m);
-            for (int j = 1; j <= ust; j++)
-                matrix[0, j] = j;
-
-            // Step 3
-
-            for (int i = 1; i <= n; i++)
-            {
-                char s_i = source[i - 1];
-                // Step 4
-                bool devamet = false;
-                if (i - limit >= 1)
-                {
-                    //		matrix[i,i-limit]=matrix[i-1,i-limit]+1;
-                    matrix[i, i - limit] = Math.Min(matrix[i - 1, i - limit] + 1,
-                            matrix[i - 1, i - limit - 1] +
-                            ((source[i - 1] == target[i - limit - 1]) ? 0 : 1));
-                    devamet |= matrix[i, i - limit] <= limit;
-                }
-
-                int alt = Math.Max(i - limit + 1, 1);
-
-                ust = Math.Min(i + limit - 1, m);
-
-                for (int j = alt; j <= ust; ++j)
-                {
-                    char t_j = target[j - 1];
-                    // Step 5
-                    int cost;
-                    if (s_i == t_j)
-                    {
-                        cost = 0;
-                    }
-                    else
-                    {
-                        cost = 1;
-                    }
-
-                    // Step 6
-                    int above = matrix[i - 1, j];
-                    int left = matrix[i, j - 1];
-                    int diag = matrix[i - 1, j - 1];
-                    int cell = Math.Min(above + 1, Math.Min(left + 1, diag + cost));
-
-                    // Step 6A: Cover transposition, in addition to deletion,
-                    // insertion and substitution. This step is taken from:
-                    // Berghel, Hal ; Roach, David : "An Extension of Ukkonen's
-                    // Enhanced Dynamic Programming ASM Algorithm"
-                    // (http://www.acm.org/~hlb/publications/asm/asm.html)
-                    /* bu kismi simdilik, geciyoruz */
-                    if (i > 1 && j > 1)
-                    {
-                        int trans = matrix[i - 2, j - 2] + 1;
-                        if (source[i - 2] != t_j) trans++;
-                        if (s_i != target[j - 2]) trans++;
-                        if (cell > trans) cell = trans;
-                    }
-                    /*		*/
-                    matrix[i, j] = cell;
-                    devamet |= cell <= limit;
-                    //		if (cell>limit) break;
-                }
-                if (i + limit <= m)
-                {
-                    matrix[i, i + limit] = Math.Min(matrix[i, i + limit - 1] + 1,
-                            matrix[i - 1, i + limit - 1] +
-                            ((source[i - 1] == target[i + limit - 1]) ? 0 : 1));
-                    devamet |= matrix[i, i + limit] <= limit;
-                }
-                if (!devamet) return disarda;
-            }
-
-            // Step 7
-
-            return matrix[n, m];
         }
 
         /**
@@ -169,46 +58,6 @@ namespace NZemberek.Cekirdek.Araclar
         public static bool inEditDistance(string source, string target, int dist)
         {
             return (editDistance(source, target, dist) <= dist);
-            //return (editDistanceMert(source, target, dist) <= dist);
-        }
-
-
-        /**
-         * Verilen s1 stringinin verilen distance düzeltme mesafesi çerçevesinde
-         * s2 stringinin alt stringi olup olmadığını döndürürr. Ã–rneğin:
-         * <pre>
-         * isInSubStringLevenshteinDistance("elma","ekmalar",1) -> true
-         * isInSubStringLevenshteinDistance("elma","emalar",1) -> true
-         * isInSubStringLevenshteinDistance("elma","eksalar",1) -> false (substring min dist=2)
-         * </pre>
-         *
-         * @param s1       :
-         * @param s2       : s1'i distance düzeltme mesafesi içinde kapsayıp kapsamadığı araştırılan String
-         * @param distance : düzeltme mesafesi
-         * @return eger istenilen mesafede is true.
-         */
-        public static bool isInSubstringEditDistance(String s1, String s2, int distance)
-        {
-            if (s2.Length < (s1.Length - distance) || s1.Length > (s2.Length + distance))
-                return false;
-
-            if (s2.Length >= s1.Length)
-            {
-                String test = s2.Substring(0, s1.Length);
-                if (inEditDistance(s1, test, distance)) return true;
-                test = s2.Substring(0, s1.Length - 1);
-                if (inEditDistance(s1, test, distance)) return true;
-                if (s2.Length >= s1.Length + 1)
-                {
-                    test = s2.Substring(0, s1.Length + 1);
-                    if (inEditDistance(s1, test, distance)) return true;
-                }
-            }
-            else if (s1.Length >= s2.Length)
-            {
-                if (inEditDistance(s1, s2, distance)) return true;
-            }
-            return false;
         }
 
         /**
@@ -236,9 +85,51 @@ namespace NZemberek.Cekirdek.Araclar
             return (jaroWinkler.benzerlikOrani(s1, s2) >= enazBenzerlik);
         }
 
-        private static int editDistanceMert(string s, string t, int limit)
+        /// <summary>
+        /// Verilen s1 stringinin verilen distance duzeltme mesafesi cercevesinde 
+        /// s2 stringinin alt stringi olup olmadigini dondurur. Ornegin:
+        ///  isInSubStringLevenshteinDistance("elma","ekmalar",1) -> true
+        ///  isInSubStringLevenshteinDistance("elma","emalar",1) -> true
+        ///  isInSubStringLevenshteinDistance("elma","eksalar",1) -> false (substring min dist=2)
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2">s1'i distance duzeltme mesafesi icinde kapsayıp kapsamadigi arastirilan String</param>
+        /// <param name="distance">duzeltme mesafesi</param>
+        /// <returns></returns>
+        public static bool isInSubstringEditDistance(String s1, String s2, int distance)
         {
-            //int curCell = 0;
+            if (s2.Length < (s1.Length - distance))
+                return false;
+
+            if (s2.Length >= s1.Length)
+            {
+                String test = s2.Substring(0, s1.Length);
+                if (inEditDistance(s1, test, distance)) return true;
+                test = s2.Substring(0, s1.Length - 1);
+                if (inEditDistance(s1, test, distance)) return true;
+                if (s2.Length >= s1.Length + 1)
+                {
+                    test = s2.Substring(0, s1.Length + 1);
+                    if (inEditDistance(s1, test, distance)) return true;
+                }
+            }
+            else
+            {
+                if (inEditDistance(s1, s2, distance)) return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Degistirilmis Levenshtein Edit Distance Algoritmasi. 
+        /// Transpozisyonları da 1 düzeltme mesafesi olarak hesapliyor.
+        /// </summary>
+        /// <param name="s">kaynak</param>
+        /// <param name="t">hedef</param>
+        /// <param name="limit">uzaklik</param>
+        /// <returns></returns>
+        private static int editDistance(string s, string t, int limit)
+        {
             int n = s.Length; //length of s
             int m = t.Length; //length of t
             int[,] d = new int[n + 1, m + 1]; // matrix
@@ -264,7 +155,7 @@ namespace NZemberek.Cekirdek.Araclar
                 }
             }
             // Step 7
-            return d[n, m] > limit ? limit+1:d[n, m];
+            return d[n, m] > limit ? limit + 1 : d[n, m];
         }
 
         private static int transpozisyonMert(string s, string t, int i, int j, int[,] d)
@@ -272,38 +163,119 @@ namespace NZemberek.Cekirdek.Araclar
             if (i > 1 && j > 1)
             {
                 int trans = d[i - 2, j - 2] + 1;
-                if (s[i - 2] != t[j - 1]) 
+                if (s[i - 2] != t[j - 1])
                     trans++;
-                if (s[i - 1] != t[j - 2]) 
+                if (s[i - 1] != t[j - 2])
                     trans++;
                 if (d[i, j] > trans) d[i, j] = trans;
             }
             return d[i, j];
         }
-
-        public static bool isInSubstringEditDistanceMert(String s1, String s2, int distance)
-        {
-            string longer = s2;
-            string shorter = s1;
-
-            if (s1.Length == s2.Length)
-            {
-                return inEditDistance(s1, s2, distance);
-            }
-
-            if (s1.Length > s2.Length)
-            {
-                longer = s1;
-                shorter = s2;
-            }
-
-            string testStr = string.Empty;
-            for (int i = 1; i < (longer.Length-shorter.Length)+distance+1; i++)
-            {
-                testStr = longer.Substring(0, longer.Length-i);
-                if (inEditDistance(s1, testStr, distance)) return true;
-            }
-            return false;
-        }
     }
 }
+
+        /////**
+        //// * Degistirilmis Levenshtein Edit Dist. algoritması. transpozisyonları da 1 düzeltme mesafesi
+        //// * olarak hesaplar. Uzaklik limit degerden buyuk olursa islem kesilir. (Alphan)
+        //// *
+        //// * @param source
+        //// * @param target
+        //// * @param limit
+        //// * @return iki kelime arasindaki mesafe, tamsayi cinsinden. kucuk rakamlar daha buyuk benzerligi gosterir.
+        //// */
+        ////public static int editDistance(String source, String target, int limit)
+        ////{
+        ////    // Step 1
+        ////    int n = source.Length;
+        ////    int m = target.Length;
+        ////    int disarda = limit + 1;
+
+        ////    if (n == 0 || m == 0)
+        ////        return Math.Max(m, n);
+
+        ////    if (Math.Abs(m - n) > limit)
+        ////        return disarda;
+
+        ////    int[,] matrix = new int[n + 1, m + 1];
+
+        ////    // Step 2
+        ////    int ust;
+        ////    ust = Math.Min(limit, n);
+        ////    for (int i = 0; i <= ust; i++)
+        ////        matrix[i, 0] = i;
+        ////    ust = Math.Min(limit, m);
+        ////    for (int j = 1; j <= ust; j++)
+        ////        matrix[0, j] = j;
+
+        ////    // Step 3
+
+        ////    for (int i = 1; i <= n; i++)
+        ////    {
+        ////        char s_i = source[i - 1];
+        ////        // Step 4
+        ////        bool devamet = false;
+        ////        if (i - limit >= 1)
+        ////        {
+        ////            //		matrix[i,i-limit]=matrix[i-1,i-limit]+1;
+        ////            matrix[i, i - limit] = Math.Min(matrix[i - 1, i - limit] + 1,
+        ////                    matrix[i - 1, i - limit - 1] +
+        ////                    ((source[i - 1] == target[i - limit - 1]) ? 0 : 1));
+        ////            devamet |= matrix[i, i - limit] <= limit;
+        ////        }
+
+        ////        int alt = Math.Max(i - limit + 1, 1);
+
+        ////        ust = Math.Min(i + limit - 1, m);
+
+        ////        for (int j = alt; j <= ust; ++j)
+        ////        {
+        ////            char t_j = target[j - 1];
+        ////            // Step 5
+        ////            int cost;
+        ////            if (s_i == t_j)
+        ////            {
+        ////                cost = 0;
+        ////            }
+        ////            else
+        ////            {
+        ////                cost = 1;
+        ////            }
+
+        ////            // Step 6
+        ////            int above = matrix[i - 1, j];
+        ////            int left = matrix[i, j - 1];
+        ////            int diag = matrix[i - 1, j - 1];
+        ////            int cell = Math.Min(above + 1, Math.Min(left + 1, diag + cost));
+
+        ////            // Step 6A: Cover transposition, in addition to deletion,
+        ////            // insertion and substitution. This step is taken from:
+        ////            // Berghel, Hal ; Roach, David : "An Extension of Ukkonen's
+        ////            // Enhanced Dynamic Programming ASM Algorithm"
+        ////            // (http://www.acm.org/~hlb/publications/asm/asm.html)
+        ////            /* bu kismi simdilik, geciyoruz */
+        ////            if (i > 1 && j > 1)
+        ////            {
+        ////                int trans = matrix[i - 2, j - 2] + 1;
+        ////                if (source[i - 2] != t_j) trans++;
+        ////                if (s_i != target[j - 2]) trans++;
+        ////                if (cell > trans) cell = trans;
+        ////            }
+        ////            /*		*/
+        ////            matrix[i, j] = cell;
+        ////            devamet |= cell <= limit;
+        ////            //		if (cell>limit) break;
+        ////        }
+        ////        if (i + limit <= m)
+        ////        {
+        ////            matrix[i, i + limit] = Math.Min(matrix[i, i + limit - 1] + 1,
+        ////                    matrix[i - 1, i + limit - 1] +
+        ////                    ((source[i - 1] == target[i + limit - 1]) ? 0 : 1));
+        ////            devamet |= matrix[i, i + limit] <= limit;
+        ////        }
+        ////        if (!devamet) return disarda;
+        ////    }
+
+        ////    // Step 7
+
+        ////    return matrix[n, m];
+        ////}
---
+++ NZemberek.TurkiyeTurkcesi/Testler/TestMetinAraclari.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NZemberek.Cekirdek.Araclar;
+
+namespace NZemberek.TurkiyeTurkcesi.Testler
+{
+    [TestFixture]
+    public class TestMetinAraclari
+    {
+        [Test]
+        public void levenstheinTest()
+        {
+            string s1 = "çekostlavakya";
+            string s2 = "cekoslavakia";
+            int expected = 3;
+            int actual = MetinAraclari.editDistance(s1, s2);
+            Assert.AreEqual(expected, actual, string.Format("{0} beklenirken {1} geldi.", expected, actual));
+        }
+
+        [Test]
+        public void testEditDistance()
+        {
+            Assert.AreEqual(0, MetinAraclari.editDistance("elma", "elma"),string.Format("{0} bekleniyordu.", 0));
+            Assert.AreEqual(1, MetinAraclari.editDistance("elma", "elmax"),string.Format("{0} bekleniyordu.", 1));
+            Assert.AreEqual(1, MetinAraclari.editDistance("elma", "lma"),string.Format("{0} bekleniyordu.", 1));
+            Assert.AreEqual(2, MetinAraclari.editDistance("elma", "ma"),string.Format("{0} bekleniyordu.", 2));
+            Assert.AreEqual(2, MetinAraclari.editDistance("elma", "frma"),string.Format("{0} bekleniyordu.", 2));
+            Assert.AreEqual(3, MetinAraclari.editDistance("elma", "a"),string.Format("{0} bekleniyordu.", 3));
+            Assert.AreEqual(3, MetinAraclari.editDistance("elma", "elmalar"),string.Format("{0} bekleniyordu.", 3));
+            Assert.AreEqual(4, MetinAraclari.editDistance("elma", "elmalara"),string.Format("{0} bekleniyordu.", 4));
+            Assert.AreEqual(4, MetinAraclari.editDistance("elma", "amel"),string.Format("{0} bekleniyordu.", 4));
+            Assert.AreEqual(5, MetinAraclari.editDistance("elma", "frtyu"),string.Format("{0} bekleniyordu.", 5));
+            // ************ TRANSPOZISYON *********************
+            Assert.AreEqual(1, MetinAraclari.editDistance("elma", "emla"),string.Format("{0} bekleniyordu.", 1));
+            Assert.AreEqual(1, MetinAraclari.editDistance("elma", "elam"),string.Format("{0} bekleniyordu.", 1));
+            Assert.AreEqual(1, MetinAraclari.editDistance("elma", "lema"),string.Format("{0} bekleniyordu.", 1));
+            Assert.AreEqual(1, MetinAraclari.editDistance("varil", "varli"),string.Format("{0} bekleniyordu.", 1));
+            Assert.AreEqual(1, MetinAraclari.editDistance("varil", "vrail"),string.Format("{0} bekleniyordu.", 1));
+            Assert.AreEqual(1, MetinAraclari.editDistance("varil", "vairl"),string.Format("{0} bekleniyordu.", 1));
+            Assert.AreEqual(1, MetinAraclari.editDistance("varil", "avril"),string.Format("{0} bekleniyordu.", 1));
+        }
+
+        [Test]
+        public void testInModifiedLevenshteinDistance()
+        {
+            Assert.IsTrue(MetinAraclari.inEditDistance("elma", "elma", 1));
+            Assert.IsTrue(MetinAraclari.inEditDistance("elma", "ekma", 1));
+            Assert.IsTrue(MetinAraclari.inEditDistance("elma", "ema", 1));
+            Assert.IsTrue(MetinAraclari.inEditDistance("elma", "elmas", 1));
+            Assert.IsTrue(MetinAraclari.inEditDistance("elma", "lma", 1));
+            Assert.IsTrue(MetinAraclari.inEditDistance("elma", "emas", 2));
+            Assert.IsTrue(MetinAraclari.inEditDistance("elma", "el", 2));
+            Assert.IsFalse(MetinAraclari.inEditDistance("elma", "el", 1));
+            Assert.IsFalse(MetinAraclari.inEditDistance("elma", "eksa", 1));
+            Assert.IsFalse(MetinAraclari.inEditDistance("armutu", "armutlr", 1));
+            Assert.IsTrue(MetinAraclari.inEditDistance("armutlar", "armutlr", 1));
+        }
+
+        //[Test]
+        //public void isInSubstringEditDistanceMert()
+        //{
+        //    Assert.IsTrue(MetinAraclari.isInSubstringEditDistanceMert("elma", "elma", 1));
+        //    Assert.IsTrue(MetinAraclari.isInSubstringEditDistanceMert("elma", "elmalar", 1));
+        //    Assert.IsTrue(MetinAraclari.isInSubstringEditDistanceMert("elma", "ekmalar", 1));
+        //    Assert.IsTrue(MetinAraclari.isInSubstringEditDistanceMert("elma", "emaciklar", 1));
+        //    Assert.IsTrue(MetinAraclari.isInSubstringEditDistanceMert("sefil", "sfil", 1));
+        //}
+
+        [Test]
+        public void testIsInSubStringEditDistance()
+        {
+            Assert.IsTrue(MetinAraclari.isInSubstringEditDistance("elma", "elma", 1));
+            Assert.IsTrue(MetinAraclari.isInSubstringEditDistance("elma", "elmalar", 1));
+            Assert.IsTrue(MetinAraclari.isInSubstringEditDistance("elma", "ekmalar", 1));
+            Assert.IsTrue(MetinAraclari.isInSubstringEditDistance("elma", "emaciklar", 1));
+            Assert.IsTrue(MetinAraclari.isInSubstringEditDistance("sefil", "sfil", 1));
+        }
+
+        [Test]
+        public void testJaroWinklerBenzerlik()
+        {
+            Assert.IsTrue(MetinAraclari.sozcukBenzerlikOrani("elma", "elm") > 0.9d);
+            Assert.IsTrue(MetinAraclari.sozcukBenzerlikOrani("elma", "elam") > 0.9d);
+            Assert.IsTrue(MetinAraclari.sozcukBenzerlikOrani("elma", "elfa") > 0.85d);
+            Assert.IsTrue(MetinAraclari.sozcukBenzerlikOrani("elma", "elmar") > 0.9d);
+        }
+    }
+}
